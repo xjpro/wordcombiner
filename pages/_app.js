549349a25135bcdefd52c5ae36acb27ecfe2758a
jpro <jprodahl@gmail.com>
2022-04-14 15:59:28 -0500

    start

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import "../styles/globals.css";
+import "bootswatch/dist/morph/bootstrap.min.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <title>Word Combiner</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <div className="container">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
 
 export default MyApp;
